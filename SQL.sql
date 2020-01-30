49bc25ff781f8830cbfc8069d85cfb4045eb371c
damosm <damosm@free.fr>
2020-01-30 13:56:49 +0100

    question 7 et 8

--- SQL.sql
+++ SQL.sql
@@ -1,2 +1,6 @@
 # test
 use dataia_Nancy;
+
+select anciennete, sinistre, count(*) from dataia_nancy group by anciennete, sinistre;
+
+select anciennete, sinistre, round(count(*) / (select count(*)from dataia_Nancy)*100) as 'pourcent' from dataia_nancy group by anciennete, sinistre;
